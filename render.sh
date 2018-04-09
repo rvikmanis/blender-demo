348c73af7273384c2e4a25d27f579707e48db045
Rudolfs Vikmanis <rvikmanis@evolutiongaming.com>
2018-04-09 20:45:02 +0300

    Remove cube rendering

--- render.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-export COLOR_R=$1
-export COLOR_G=$2
-export COLOR_B=$3
-
-BLENDER_PATH=/Applications/Blender/blender.app/Contents/MacOS/blender
-
-${BLENDER_PATH} -b ./cube.blend -P generate_image.py
