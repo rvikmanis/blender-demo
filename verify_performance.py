08cb9a142b1fcf7335577d8b679026ea83d34acd
Rudolfs Vikmanis <rvikmanis@evolutiongaming.com>
2018-04-11 04:52:59 +0300

    Replace assertion with accumulative error logging

--- verify_performance.py
+++ verify_performance.py
@@ -1,5 +1,15 @@
 import json
 
+
+errors = []
+
+
+def log_error_if_not(predicate, message):
+    global errors
+    if predicate:
+        errors += [message]
+
+
 with open("variants.json") as variants_file:
     variants = json.load(variants_file)
 
@@ -9,8 +19,12 @@ with open("output/performance.json") as performance_log_file:
 for (name, variant) in variants.items():
     max_time = variant["max_allowed_render_time"]
     actual_time = performance_log[name]
-    assert actual_time <= max_time, (
+    log_error_if_not(
+        actual_time <= max_time,
         "%s rendered in %s seconds which exceeds the max allowed time of %s" % (name, actual_time, max_time)
     )
 
-print("Performance OK.")
+if not errors:
+    print("Performance OK.")
+else:
+    print("\n".join(errors))
